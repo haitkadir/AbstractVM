f7c5f81e9e2b2e403e07e9d45b564c815a84646c
haitkadir <hssain.aitkadir@gmail.com>
2025-01-02 01:14:02 +0100

    progressing on Lexer part

--- include/Lexer.hpp
+++ include/Lexer.hpp
@@ -1,38 +1,133 @@
 #ifndef __LEXER_H__
 #define __LEXER_H__
 
+#include <regex>
 #include <string>
 #include <vector>
-#include <regex>
 #include <stdexcept>
+#include <iostream>
+#include <sstream>
 
-
-class Lexer{
-private:
-    std::string input_;
-    size_t current_line_ = 1;
-    
-    static TokenType identifyToken(const std::string &str);
-    static bool isValue(const std::string &str);
-public:
-    Lexer ();
-    Lexer (const Lexer &a);
-    Lexer & operator = (const Lexer &a);
-    ~Lexer ();
-
-    enum TokenType {
-        PUSH, POP, DUMP, ASSERT, ADD, SUB, MUL, DIV, MOD, PRINT, EXIT, VALUE, COMMENT, SEP, INVALID
-    };
-
-    struct Token {
-        TokenType type;
-        std::string value;
-        size_t line;
-    };
-
-    explicit Lexer(const std::string &input);
-    std::vector<Token> tokenize();
+enum TokenType { PUSHINT8, PUSHINT16, PUSHINT32, PUSHINT, PUSHFLOAT, PUSHDOUBLE, POP, DUMP, ASSERT, ADD, SUB, MUL, DIV, MOD, PRINT, EXIT, COMMENT, SEP, INVALID };
+struct Token {
+    TokenType type;
+    std::string value;
+    size_t line;
 };
+class Lexer {
+public:
+    
+
+    Lexer() : _input(""), _line(1) {}
+    // Lexer(const std::string& input) : _input(input), _line(1) {}
+
+    std::vector<Token> tokenize(const std::string& input) {
+        this->_input = input;
+        std::istringstream streamfile(input);
+        std::string line;
+        std::vector<Token> tokens;
+
+        while (std::getline(streamfile, line)) {
+
+            std::cout << "before{" << line << "}" << std::endl;
+            if (std::regex_match(line, std::regex(R"(^\s+$)")) || std::regex_match(line, std::regex(R"(^;.*$)"))){
+                std::cout << "spaces catched" << std::endl;
+                ++_line;
+                continue;
+            }
+            std::cout << "after{" << line << "}" << std::endl;
+            Token token;
+            Token temp;
+            temp = identifyToken(line);
+            token.value = line;
+            token.type = temp.type;
+            token.value = temp.value;
+            std::cout << "toketype: " << token.type << std::endl;
+            std::cout << "tokeValue: " << token.value << std::endl;
+            token.line = _line;
+
+            if (token.type == INVALID) {
+                throw std::runtime_error("Lexical error on line " + std::to_string(_line));
+            }
+            tokens.push_back(token);
+            ++_line;
+        }
+        return tokens;
+    }
+
+private:
+    std::string _input;
+    size_t _line;
+
+    Token identifyToken(const std::string& value) {
+        Token token;
+        std::smatch match;
+        if (std::regex_match(value, match, std::regex(R"(^push\s+int8\((\d+)\)$)"))){
+            token.type = PUSHINT8;
+            token.value = match[1];
+            return token;
+        }
+        if (std::regex_match(value, match, std::regex(R"(^push\s+int16\((\d+)\)$)"))){
+            token.type = PUSHINT16;
+            token.value = match[1];
+            return token;
+        }
+        if (std::regex_match(value, match, std::regex(R"(^push\s+int32\((\d+)\)$)"))){
+            token.type = PUSHINT32;
+            token.value = match[1];
+            return token;
+        }
+        if (std::regex_match(value, match, std::regex(R"(^push\s+int\((\d+)\)$)"))){
+            token.type = PUSHINT;
+            token.value = match[1];
+            return token;
+        }
+        if (std::regex_match(value, match, std::regex(R"(^push\s+float\((\d+\.\d+)\)$)"))){
+            token.type = PUSHFLOAT;
+            token.value = match[1];
+            return token;
+        }
+        if (std::regex_match(value, match, std::regex(R"(^push\s+double\((\d+\.\d+)\)$)"))){
+            token.type = PUSHDOUBLE;
+            token.value = match[1];
+            return token;
+        }
+        if (std::regex_match(value, match, std::regex(R"(^assert\s+int8\((\d+)\)$)"))){
+            token.type = ASSERTINT8;
+            token.value = match[1];
+            return token;
+        }
+        if (std::regex_match(value, match, std::regex(R"(^assert\s+int16\((\d+)\)$)"))){
+            token.type = ASSERTINT16;
+            token.value = match[1];
+            return token;
+        }
+        if (std::regex_match(value, match, std::regex(R"(^assert\s+int32\((\d+)\)$)"))){
+            token.type = ASSERTINT32;
+            token.value = match[1];
+            return token;
+        }
+        if (std::regex_match(value, match, std::regex(R"(^assert\s+int\((\d+)\)$)"))){
+            token.type = ASSERTINT;
+            token.value = match[1];
+            return token;
+        }
+        if (std::regex_match(value, match, std::regex(R"(^assert\s+float\((\d+\.\d+)\)$)"))){
+            token.type = ASSERTFLOAT;
+            token.value = match[1];
+            return token;
+        }
+        if (std::regex_match(value, match, std::regex(R"(^assert\s+double\((\d+\.\d+)\)$)"))){
+            token.type = ASSERTDOUBLE;
+            token.value = match[1];
+            return token;
+        }
+
+        token.type = INVALID;
+        return token;
+    }
+};
+
 
 #endif
 
---
+++ tests/Lexer-tests.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "../include/Lexer.hpp"
+#include <fstream>
+#include <sstream>
+#include <string>
+
+std::string readFileToString(const std::string& filePath) {
+    std::ifstream file(filePath); // Open the file
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open the file: " + filePath);
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf(); // Read file buffer into stringstream
+    return buffer.str(); // Convert stringstream to string
+}
+
+ 
+int main()
+{
+    try{
+        Lexer lexer;
+        std::string filePath = "../programs/exemple.avm";
+        std::string content = readFileToString(filePath);
+        std::vector<Token> tokens = lexer.tokenize(content);
+        std::cout << "===================================================================================================" << std::endl;;
+        for (auto token : tokens){
+            std::cout << "{" << std::endl;
+            std::cout << "  type : " << token.type << std::endl;
+            std::cout << "  value: " << token.value << std::endl;
+            std::cout << "}" << std::endl;
+        }
+    } catch (std::exception &e){
+        std::cout << e.what() << std::endl;
+    }
+    return(0);
+}
+
+
